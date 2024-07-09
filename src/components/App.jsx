01bcfff71d95bb9adadeb6dbeab98c6a5583267a
unknown <areg.sarkisyan.99@mail.ru>
2024-07-09 14:37:10 +0300

    test commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -86,6 +86,7 @@ function App() {
   return (
     <div className='page'>
       <Routes>
+        <Route path='/noviy-kovcheg' element={<MainPage isScrolled={isScrolled} isScreenMid={isScreenMid} isScreenSmall={isScreenSmall}/>}/>
         <Route path='/' element={<MainPage isScrolled={isScrolled} isScreenMid={isScreenMid} isScreenSmall={isScreenSmall}/>}/>
         <Route path='/red-lounge' element={<LoungePage isScrolled={isScrolled} info={loungesInfo.red} isScreenMid={isScreenMid} isScreenSmall={isScreenSmall}/>}/>
         <Route path='/small-lounge' element={<LoungePage isScrolled={isScrolled} info={loungesInfo.small} isScreenMid={isScreenMid} isScreenSmall={isScreenSmall}/>}/>
